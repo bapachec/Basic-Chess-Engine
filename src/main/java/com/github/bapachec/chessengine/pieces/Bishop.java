fe219c32325bf9f3b0742939fa49c8ff5772a117
Brandon Pacheco <BrandonAPacheco@Gmail.com>
2024-08-27 13:29:16 -0400

    added bishop legal moves except capture function

--- src/main/java/com/github/bapachec/chessengine/pieces/Bishop.java
+++ src/main/java/com/github/bapachec/chessengine/pieces/Bishop.java
@@ -5,6 +5,18 @@ public class Bishop extends Piece {
     public Bishop(boolean isWhite, int row, int column) {
         super(isWhite, row, column);
     }
+
+    @Override
+    public boolean isLegalMove(int row, int column) {
+        if (!super.isLegalMove(row, column))
+            return false;
+
+        if (Math.abs(row - getRow()) == 0 || Math.abs(column - getColumn()) == 0)
+            return false;
+
+        return true;
+    }
+
     @Override
     public String toString() { return "B"; }
 }
