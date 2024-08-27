8635ebd93cff0523370b47cd21a0b001b77729a5
Brandon Pacheco <BrandonAPacheco@Gmail.com>
2024-08-27 12:47:05 -0400

    added rook legal moves except capture function

--- src/main/java/com/github/bapachec/chessengine/pieces/Rook.java
+++ src/main/java/com/github/bapachec/chessengine/pieces/Rook.java
@@ -6,6 +6,16 @@ public class Rook extends Piece {
         super(isWhite, row, column);
     }
 
+    @Override
+    public boolean isLegalMove(int row, int column) {
+        if (!super.isLegalMove(row, column))
+            return false;
+
+        if (Math.abs(row - getRow()) != 0 && Math.abs(column - getColumn()) != 0)
+            return false;
+
+        return true;
+    }
 
     @Override
     public String toString() {
